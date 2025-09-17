8a2cbdeabfb5ba400368cab4405bb9345aed6059
Javier Cicchelli <javier@rock-n-code.com>
2025-09-17 19:18:48 +0200

    Added the hummingbird package dependency to the Package file ans attached its products to the library and tests targets.

--- Package.swift
+++ Package.swift
@@ -17,15 +17,22 @@ let package = Package(
             targets: [DocCMiddleware.target]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0")
+    ],
     targets: [
         .target(
             name: DocCMiddleware.target,
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird"),
+            ],
             path: "Sources/DocCMiddleware",
             swiftSettings: [.enableExperimentalFeature("StrictConcurrency=complete")]
         ),
         .testTarget(
             name: DocCMiddleware.test,
             dependencies: [
+                .product(name: "HummingbirdTesting", package: "hummingbird"),
                 .byName(name: DocCMiddleware.target)
             ],
             path: "Tests/DocCMiddleware"
