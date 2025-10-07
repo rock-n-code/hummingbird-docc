0e71b2673c4eab57f16d298a412fdc31b63e3bba
Javier Cicchelli <javier@rock-n-code.com>
2025-10-07 23:35:55 +0000

    Update license to Apache v2.0 (#6)
    This PR contains the work done to update the project to use the *Apache  v2.0* license.
    
    Reviewed-on: https://repo.rock-n-code.com/rock-n-code/hummingbird-docc/pulls/6
    Co-authored-by: Javier Cicchelli <javier@rock-n-code.com>
    Co-committed-by: Javier Cicchelli <javier@rock-n-code.com>

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
 // swift-tools-version: 6.0
 
+// ===----------------------------------------------------------------------===
+// 
+// This source file is part of the Hummingbird DocC open source project
+// 
+// Copyright (c) 2025 Röck+Cöde VoF. and the Hummingbird DocC project authors
+// Licensed under Apache license v2.0
+// 
+// See LICENSE for license information
+// See CONTRIBUTORS for the list of Hummingbird DocC project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+// 
+// ===----------------------------------------------------------------------===
+
 import PackageDescription
 
 let package = Package(
--- Samples/HummingbirdDocC/App/Builders/AppBuilder.swift
+++ Samples/HummingbirdDocC/App/Builders/AppBuilder.swift
@@ -1,13 +1,15 @@
 // ===----------------------------------------------------------------------===
-//
-// This source file is part of the Hummingbird DocC Middleware open source project
-//
-// Copyright (c) 2025 Röck+Cöde VoF. and the Hummingbird DocC Middleware project authors
-// Licensed under the EUPL 1.2 or later.
-//
+// 
+// This source file is part of the Hummingbird DocC open source project
+// 
+// Copyright (c) 2025 Röck+Cöde VoF. and the Hummingbird DocC project authors
+// Licensed under Apache license v2.0
+// 
 // See LICENSE for license information
-// See CONTRIBUTORS for the list of Hummingbird DocC Middleware project authors
+// See CONTRIBUTORS for the list of Hummingbird DocC project authors
 //
+// SPDX-License-Identifier: Apache-2.0
+// 
 // ===----------------------------------------------------------------------===
 
 import class Hummingbird.Router
